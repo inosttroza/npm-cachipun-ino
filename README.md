fad4a42d28655c4b7737893a7169f32ac1c199d6
inosttroza <inosttroza@gmail.com>
2021-10-01 13:03:16 -0300

    update files

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # cachipun-ino
 
-```
 Retorna piedra, papel o tijera desde el lado del servidor.
 Desarrollo subido a NPM(Node Package Manager) con el nombre de 'cachipun-ino'
-```
+
 
 **Ejemplo:**
 ```
 const {eleccionServer} = require('cachipun-ino');
+console.log(eleccionServer());
+o
+const eleccionServer = require('cachipun-ino').eleccionServer();
 console.log(eleccionServer);
 ```
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 //desarrollo que retorna piedra, papel o tijera desde el lado del server
-function eleccionServer(){
+const eleccionServer = () =>{
     const number = Math.floor(Math.random() * 3);
     switch (number) {
         case 0:
-            return "PIEDRA";
+            return "piedra";
         case 1:
-            return "PAPEL";
+            return "papel";
         case 2:
-            return "TIJERA";
+            return "tijera";
     }
 }
 
